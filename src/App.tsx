4b1fe7c71c9d0f1f6e2251ae8922c10206974e5a
Shota Shimazu <shotastage@users.noreply.github.com>
2021-07-14 19:42:51 +0900

    Replace default page content

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 
-function App() {
+interface Props {}
+
+const App: React.FC<Props> = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Bhaa Beta Program</h1>
+      <p>
+        This is a pen.
+      </p>
+      <input placeholder="Apple ID" />
+      <input placeholder="Message (Optional)" />
+      <button>Submit</button>
+    </>
   );
 }
 

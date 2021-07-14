da37ee914a6c6b75aa9f94df2cafde4d420a1134
Shota Shimazu <shotastage@users.noreply.github.com>
2021-07-15 00:48:50 +0900

    Add get method

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,6 +1,17 @@
 const XHR = new XMLHttpRequest();
 
+const DefaultHeaders = [
+    ["Content-Type", "application/json"]
+];
+
 class ApiService {
+
+    get(url: string) {
+        XHR.open("GET", url);
+        XHR.setRequestHeader("Content-Type", "application/json");
+        XHR.send();
+    }
+
     post(url: string, data: { [key: string]: any; }) {
         XHR.open("POST", url);
         XHR.setRequestHeader("Content-Type", "application/json");
